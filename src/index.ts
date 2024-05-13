408dfb44023380423dd5f7e50049e2e5b45eea20
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-05-14 03:09:25 +0900

    :sparkles: feat: add new ts file to generate and store password

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import generator from 'generate-password-ts';
+
+const password = generator.generate({
+  length: 10,
+  numbers: true,
+});
+
+const tsPath = "./test/test.ts";
+const tsText = `const password = "${password}";`;
+
+await Bun.write(tsPath, tsText);
