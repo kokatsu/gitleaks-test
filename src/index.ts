854bfe575a8ca0587c336176045466a5637735b2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-05-15 09:52:47 +0900

    :sparkles: feat: add new json file to generate and store password

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,11 @@ const password = generator.generate({
 const tsPath = './test/test.ts';
 const tsText = `const password = '${password}';`;
 
+const jsonPath = './test/test.json';
+const jsonObject = {
+  password: password,
+};
+const jsonText = JSON.stringify(jsonObject);
+
 await Bun.write(tsPath, tsText);
+await Bun.write(jsonPath, jsonText);
