260900f220ad3375aff198323657a3f0190b465b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-05-22 16:36:07 +0900

    :bug: fix: amplify pull

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,23 @@
-// export type AmplifyDependentResourcesAttributes = {};
+export type AmplifyDependentResourcesAttributes = {
+  auth: {
+    gitleakstest45299d7d: {
+      AppClientID: 'string';
+      AppClientIDWeb: 'string';
+      CreatedSNSRole: 'string';
+      HostedUIDomain: 'string';
+      OAuthMetadata: 'string';
+      UserPoolArn: 'string';
+      UserPoolId: 'string';
+      UserPoolName: 'string';
+    };
+  };
+  function: {
+    gitleakstest45299d7dPreSignup: {
+      Arn: 'string';
+      LambdaExecutionRole: 'string';
+      LambdaExecutionRoleArn: 'string';
+      Name: 'string';
+      Region: 'string';
+    };
+  };
+};
